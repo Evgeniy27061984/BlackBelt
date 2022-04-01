db019fd37e240175e9954159fbd44bd10e7753ea
Евгений Обанин <89634682728@mail.ru>
2022-04-01 20:51:20 +0500

    twenty and twenty one lessons Big O notation and Vector

---
+++ src/CourcePartII/Lesson21Vector/Test28.java
@@ -0,0 +1,19 @@
+package CourcePartII.Lesson21Vector;
+
+import java.util.Vector;
+
+public class Test28 {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+        vector.add("Alex");
+        vector.add("Maxim");
+        vector.add("Alina");
+        vector.add("Svetlana");
+        System.out.println(vector);
+        System.out.println(vector.firstElement());
+        System.out.println(vector.lastElement());
+        vector.remove(2);
+        System.out.println(vector);
+        System.out.println(vector.get(1));
+    }
+}
