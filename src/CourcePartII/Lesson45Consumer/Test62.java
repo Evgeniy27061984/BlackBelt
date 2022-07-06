17a4a2c932c754a1748739a40aad19c47fcd89cf
Евгений Обанин <89634682728@mail.ru>
2022-07-06 15:25:54 +0500

    forty five lesson Consumer

--- src/CourcePartII/Lesson45Consumer/Test62.java
+++ src/CourcePartII/Lesson45Consumer/Test62.java
@@ -1,35 +1,22 @@
-package CourcePartII.Lesson44Supplier;
+package CourcePartII.Lesson45Consumer;
 
 import java.util.ArrayList;
 import java.util.function.Supplier;
-import java.util.function.Consumer;
-public class Test61 {
-    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
-        ArrayList<Car> a1 = new ArrayList<>();
+
+public class Test62 {
+    public static ArrayList<Car> createThreeCars(Supplier<   Car> carSupplier) {
+        ArrayList<   Car> a1 = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             a1.add(carSupplier.get());
         }
         return a1;
     }
 
-    public static void changeCar(Car car, Consumer<Car> carConsumer) {
-        carConsumer.accept(car);
-    }
-
     public static void main(String[] args) {
-        ArrayList<Car> ourCars = createThreeCars(() ->
-                new Car("Polo", "white", 1.6));
-        System.out.println(ourCars);
-
-        changeCar(ourCars.get(0)
-                , car -> {
-                    car.color = "red";
-                    car.engine = 2.4;
-                    System.out.println(car);
-                });
+        ArrayList<   Car> ourCars = createThreeCars(() ->
+                new    Car("Polo", "white", 1.6));
         System.out.println(ourCars);
     }
-
 }
 
 class Car {
