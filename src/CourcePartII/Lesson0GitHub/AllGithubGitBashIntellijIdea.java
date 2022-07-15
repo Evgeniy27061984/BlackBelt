0a3d0f6c5e211baaf84fc96f2ffbb97e4f0aec5d
Евгений Обанин <89634682728@mail.ru>
2022-07-15 19:52:36 +0500

    zero lesson GitHub GitBash

---
+++ src/CourcePartII/Lesson0GitHub/AllGithubGitBashIntellijIdea.java
@@ -0,0 +1,8 @@
+package CourcePartII.Lesson0GitHub;
+
+public class AllGithubGitBashIntellijIdea {
+    //cd c:
+    //ctrl+alt+i - форматирование по правилам
+    // 1) git reset --hard commit
+    // 2) git push --force origin master откат в GitHub
+}
