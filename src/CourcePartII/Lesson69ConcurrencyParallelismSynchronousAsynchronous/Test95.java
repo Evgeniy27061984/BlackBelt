57ac38ccb6ca22d73428613301cc39179d318e03
Евгений Обанин <89634682728@mail.ru>
2022-07-24 20:42:38 +0500

    sixty nine lesson ConcurrencyParallelismSynchronousAsynchronous

---
+++ src/CourcePartII/Lesson69ConcurrencyParallelismSynchronousAsynchronous/Test95.java
@@ -0,0 +1,13 @@
+package CourcePartII.Lesson69ConcurrencyParallelismSynchronousAsynchronous;
+
+
+
+public class Test95 {
+    public static void main(String[] args) {
+        //Concurrency - кушает и поет
+        //Parallelism - готовит и говорит
+        //Synchronous - строго действия по порядку
+        //Asynchronous - стирать и готовить бутерброд Предсказать вывод невозможно
+    }
+}
+
